3088a7de103e33a76dcda8bef6a24b529a268191
caioosm <matossousacaio.cms@gmail.com>
2025-11-13 20:09:07 -0300

    correcao de bugs nos buttons e no ul de projetos desenvolvidos

--- src/app/modules/portfolio/components/knowledge/knowledge.ts
+++ src/app/modules/portfolio/components/knowledge/knowledge.ts
@@ -10,24 +10,24 @@ import { IKnowledge } from '../../interface/IKnowledge.interface';
 export class Knowledge {
   public arrayKnowledge = signal<IKnowledge[]>([
     {
-      src: 'assets/icons/knowledge/angular.svg',
-      alt: 'Icone de conhecimento de Angular'
-    },
-    {
-      src: 'assets/icons/knowledge/aws.svg',
-      alt: 'Icone de conhecimento de AWS'
-    },
-    {
-      src: 'assets/icons/knowledge/dck.svg',
-      alt: 'Icone de conhecimento de Docker'
-    },
-    {
       src: 'assets/icons/knowledge/java.svg',
       alt: 'Icone de conhecimento em Java'
     },
     {
       src: 'assets/icons/knowledge/javascript.svg',
       alt: 'Icone de conhecimento em Javascript'
+    },
+    {
+      src: 'assets/icons/knowledge/angular.svg',
+      alt: 'Icone de conhecimento de Angular'
+    },
+    {
+      src: 'assets/icons/knowledge/dck.svg',
+      alt: 'Icone de conhecimento de Docker'
+    },
+    {
+      src: 'assets/icons/knowledge/aws.svg',
+      alt: 'Icone de conhecimento de AWS'
     },
     {
       src: 'assets/icons/knowledge/linux.svg',
